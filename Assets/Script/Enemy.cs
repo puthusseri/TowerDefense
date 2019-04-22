839411fccea145f8daa5a95de28616a2447b482d
puthusseri <vyshakputhusseri@gmail.com>
2019-04-22 12:15:08 +0530

    enemy moves , but only to the first waypoint

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   public float speed = 10f;
+   private Transform target;
+   private int wavepointIndex = 0;
+   
+   void Start() {
+	   target = WayPoints.points[0];
+   }
+   void Update() {
+	   Vector3 dir = target.position - transform.position;
+	   transform.Translate(dir.normalized * speed * Time.deltaTime);
+   
+   }
+   
+	   
+}
